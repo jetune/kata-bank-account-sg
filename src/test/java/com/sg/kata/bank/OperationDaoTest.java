b0c92fd7411be7c9a24857ddc44aa9636185dee4
Jean-Jacques ETUNE NGI <jean.jacques.x.etune.ngi.-nd@disney.com>
2021-01-11 18:42:51 +0100

    Add Some Test cases

--- src/test/java/com/sg/kata/bank/OperationDaoTest.java
+++ src/test/java/com/sg/kata/bank/OperationDaoTest.java
@@ -3,6 +3,11 @@
  */
 package com.sg.kata.bank;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +16,14 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.sg.kata.bank.dao.AccountDao;
+import com.sg.kata.bank.dao.ClientDao;
 import com.sg.kata.bank.dao.OperationDao;
 import com.sg.kata.bank.exception.BankAccountException;
+import com.sg.kata.bank.model.Account;
+import com.sg.kata.bank.model.Client;
 import com.sg.kata.bank.model.Operation;
+import com.sg.kata.bank.model.constants.OperationType;
 
 /**
  * Test Class for Operation DAO
@@ -33,6 +43,18 @@ public class OperationDaoTest {
 	 */
 	@Autowired
 	private OperationDao operationDao;
+
+	/**
+	 * Account DAO
+	 */
+	@Autowired
+	private AccountDao accountDao;
+
+	/**
+	 * Client DAO
+	 */
+	@Autowired
+	private ClientDao clientDao;
 	
 	/**
 	 * Method used to test the save Operation with null operation
@@ -47,13 +69,69 @@ public class OperationDaoTest {
 	/**
 	 * Method used to test the save Operation with no account number
 	 */
-	@Test(expected = BankAccountException.class)
+	@Test
 	public void testSaveOperation_No_Account_Number() {
 		
 		// Build a operation
 		Operation opetation = Operation.builder().accountNumber("").build();
 		
+		try {
+			
+			// Try to save null operation
+			operationDao.save(opetation);
+			
+		} catch (BankAccountException e) {
+			
+			// Assertion
+			assertEquals("SAVE_OPERATION_INVALID_OPERATION_ACCOUNT", e.getCode());
+		}	
+	}
+
+	/**
+	 * Method used to test the save Operation with no account number
+	 */
+	@Test
+	public void testSaveOperation_OK() {
+		
+		// Client ID
+		String clientId = "CLT001";
+		
+		// Create Client
+		clientDao.save(Client.builder()
+			.id(clientId)
+			.email("jetune@kube-cloud.com")
+			.lastName("ETUNE")
+			.firstName("JJ").build()
+		);
+		
+		// Account Number
+		String accountNumber = "ACC001";
+		
+		// Create Account
+		accountDao.save(Account.builder()
+			.number(accountNumber)
+			.clientId(clientId)
+			.build()
+		);
+		
+		// Build a operation
+		Operation opetation = Operation.builder()
+				.accountNumber(accountNumber)
+				.amount(10000)
+				.timestamp(LocalDateTime.now())
+				.type(OperationType.DEPOSIT)
+				.build();
+
 		// Try to save null operation
 		operationDao.save(opetation);
+		
+		// Find the operation
+		List<Operation> operations = operationDao.findByAccountNumber(accountNumber);
+		
+		// Checks
+		assertEquals(1, operations.size());
+		assertEquals(accountNumber, operations.get(0).getAccountNumber());
+		assertEquals(10000, operations.get(0).getAmount());
+		assertEquals(OperationType.DEPOSIT, operations.get(0).getType());
 	}
 }
